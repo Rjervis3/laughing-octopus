b51c5404e17929aa4b6047672c038efd14623569
rjervis33@me.com <jervisre@grinnell.edu>
2014-10-21 00:04:04 -0500

    Program prints n and nt factors

--- numberAnalysis.c
+++ numberAnalysis.c
@@ -1,12 +1,12 @@
-  /***********************************************************************
+     /**********************************************************************
      * Name: Renn Jervis                                                   *
-     * Box(s):                                                             *
-     * Assignment name (Lab, Project, Sup. Problem Number _______)         *
-     *      (25% off if name/number does not match assignment)             *
-     * Assignment for <due date>                                           *
+     * Box: 3762                                                           *
+     * Supplememntal Problem 6: Defective, Perfect,                        *  
+     * , and Superperfect Numbers                                          *
+     * Assignment for ...                                                  *
      ***********************************************************************/
 
-    /* *********************************************************************
+     /**********************************************************************
      * Academic honesty certification:                                     *
      *   Written/online sources used:                                      *
      *     [include textbook(s), CSC 161 labs or readings;                 *
@@ -26,3 +26,42 @@
 
 
 /* Supplemental problem 6 */
+
+
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+{
+  //read and print a max number
+  int max;
+  printf("This prgram will list certain requrements for numbers 1 to n\n");
+  printf("Please enter a integer number n:");
+  scanf("%d", &max);          //store entered number in max
+  printf("The number you entered for n was: %d\n", max);
+
+  //print the table of numbers
+  printf("The classification of numbers:\n\n");
+  printf("n : non-trivial factors : sum of proper factors : classification\n");
+
+  //create these data for numbers 1 to n and print in table form
+  int i, j, sum;
+  int factors[max];  //array to hold factors
+  for(i=1; i<=max; i++)          //first loop to go 1 to n
+    { printf(" %d :" , i);
+      for (j=1; j<i; j++)            //second loop to test each factor 1 to n
+      {
+	if ((i % j)==0 && j!=1)              //factor if j divides i evenly
+	  {
+	    factors[j-1]=j;
+	    printf("%d,", factors[j-1]);
+	    sum+=factors[j-1];
+	  }
+      }
+      printf("\n");
+    }
+}
