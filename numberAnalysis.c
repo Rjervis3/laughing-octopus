2483ee04bfcc66b7ef76a6c59a6ba15c82779514
rjervis33@me.com <jervisre@grinnell.edu>
2014-10-20 23:29:28 -0500

    Start of project

---
+++ numberAnalysis.c
@@ -0,0 +1,28 @@
+  /***********************************************************************
+     * Name: Renn Jervis                                                   *
+     * Box(s):                                                             *
+     * Assignment name (Lab, Project, Sup. Problem Number _______)         *
+     *      (25% off if name/number does not match assignment)             *
+     * Assignment for <due date>                                           *
+     ***********************************************************************/
+
+    /* *********************************************************************
+     * Academic honesty certification:                                     *
+     *   Written/online sources used:                                      *
+     *     [include textbook(s), CSC 161 labs or readings;                 *
+     *       complete citations for Web or other written sources           *
+     *      write "none" if no sources used]                               *
+     *   Help obtained                                                     *
+     *     [indicate names of instructor, class mentors                    *
+     *      or evening tutors, consulted according to class policy;        *
+     *      write "none" if none of these sources used]                    *
+     *     ["none" required for Supplemental Problems                      *
+     *   My/our signature(s) below confirms that the above list of sources *
+     *   is complete AND that I/we have not talked to anyone else          *
+     *   (e.g., CSC 161 students) about the solution to this problem       *
+     *                                                                     *
+     *   Signature:                                                        *
+     ***********************************************************************/
+
+
+/* Supplemental problem 6 */
