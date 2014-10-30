775526f49e27ecf61fe52d83725f8572983ede53
Renn Jervis <jervisre@grinnell.edu>
2014-10-29 21:29:31 -0500

    fixed printing problem, finished

--- numberAnalysis.c
+++ numberAnalysis.c
@@ -10,12 +10,15 @@
      * Academic honesty certification:                                     *
      *   Written/online sources used:                                      *
      *                                                                     *
-     *       checked :                                                     *
-     *     http://stackoverflow.com/questions/1496313/                     *
+     *       referenced:                                                   *
+     *    http://stackoverflow.com/questions/1496313/                      *
      *                           returning-c-string-from-a-function        *
-     *    for info on how to return a sting from a funct                   *
+     *    for info on how to return a sting from a funct and               *
+     *    http://stackoverflow.com/questions/18410234/                     *
+     *                how-does-one-represent-the-empty-char                *
+     *    for infor on null characters and space characters
      *   Help obtained                                                     *
-     *     [none]                                                          *
+     *     [none]  [talked to prof walker about printing]                  *
      *   My/our signature(s) below confirms that the above list of sources *
      *   is complete AND that I/we have not talked to anyone else          *
      *   (e.g., CSC 161 students) about the solution to this problem       *
@@ -39,44 +42,44 @@ char* classification (int number, int factor_sum);
 //                     and classification 
 int main()
 {
-  //read and print a max number
-  int max;
+ 
   printf("This program will list certain requrements for numbers 1 to n\n");
   printf("Please enter a integer number n: ");
+
+ //read and print a max number
+  int max;
   scanf("%d", &max);          //store entered number in max
   printf("The number you entered for n was: %d\n\n", max);
 
   //print the table of numbers
   printf("The classification of numbers:\n");
-  printf("n : non-trivial factors : sum of proper factors : classification\n");
+  printf(" n : non-trivial factors : sum of proper factors : classification\n");
 
   //create these data for numbers 1 to n and print in table form
   int i, j, factor, sum=1;
-  char leading=0;
+  char leading='\0' ;
   for(i=1; i<=max; i++)          //first loop to go 1 to n
     {
      
-
-      printf("%2d :" , i);            //print n :
+      printf("%2d :%c" , i, leading);            //print n :
       if (i==1)	sum=0; 
       else sum =1;
-      printf("%c", leading);
       for (j=1; j<i; j++)            //second loop to test each factor 1 to n-1
 	{                          
 	if ((i % j)== 0 && j !=1)          //j a factor if j divides i exactly
-	  { leading=',';
+	  { 
 	    factor=j;
-	    printf("%2d", factor);      //print nontrivial factor
+	    printf("%c%2d", leading, factor);      //print nontrivial factor
 	    // printf(" ");
 	    sum+=factor;  //add each non-trivial factor to sum
-            printf("%c", leading);
-	  } 
-        leading = 0;
+            leading=',';
+           } 
+        //leading = 0;
 
 	}
       // printf("\b ");      //delete comma after last factor
-        
-      printf(": %d ", sum);     
+      leading='\0';
+      printf(" : %d ", sum);     
       printf(": %s", classification(i, sum));     
       printf("\n");
     }
